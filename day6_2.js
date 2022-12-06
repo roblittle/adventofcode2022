de35cb6669a4986a090ee828ed37a7c55e896fec
Robert Little <roblittle@Roberts-MBP.modem>
2022-12-06 16:29:56 +0800

    easy peasy lemmon squeezy

---
+++ day6_2.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+let args = process.argv.slice(2);
+let input = fs.readFileSync(args[0]).toString();
+
+function startOfPacket(length){
+    let res
+    for(let i=length;i<input.length;i++){
+        let searchWindow = input.slice(i-length,i+1)
+        if(!/(.).*\1/.test(searchWindow)){
+            res = i+1
+            i = input.length
+        }
+    }
+    console.log(res);
+}
+
+startOfPacket(13);
+
+// TODO try sliding window approach 
+
+
