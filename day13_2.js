2b82cc11c8fbada3feea3ffd4fc17aea1a060e47
Robert Little <roblittle@Roberts-MBP.modem>
2022-12-15 14:29:22 +0800

    adding comments for easier readable contect

--- day13_2.js
+++ day13_2.js
@@ -1,21 +1,30 @@
-
 const pairs = require("fs").readFileSync(`./day13_input`, "utf-8").split("\n\n");
+
+// Alot simpler than a formal number-checking function!  thanks google/github suggestions!
 let num = Number.isFinite;
+
+// The work horse... determine if comparables are same (ie: numbers or objects)
 function compare(l, r){
+        // base case, both numbers, return comparison result as +ve == in order, -ve not
 		if (num(l) && num(r)) {
 			if (l < r) return 1;
 			if (l > r) return -1;
+        // only left side number, right side is object - make left object and re-compare
 		} else if (num(l)) return compare([l], r);
+        // only right side is number, left side is object - make right object and re-compare
 		else if (num(r)) return compare(l, [r]);
+        // nothing is a number, its either undefined or an object.... 
 		else {
+            
 			if (l === undefined) return 1;
 			else if (r === undefined) return -1;
-
+            // things are objects here... so, lets compare each internal value of the object
 			for (let i = 0; i < Math.max(l.length, r.length); i++) {
 				let comp = compare(l[i], r[i], i);
 				if (comp !== 0) return comp;
 			}
 
+            // final in-object comoparison, maintaing left smaller than right as +ve
 			if (l.length < r.length) return 1;
 			if (l.length > r.length) return -1;
 		}
@@ -28,12 +37,15 @@ let inOrder = 0;
 for (let j = 0; j < pairs.length; j++) {
 	let pair = pairs[j].split("\n");
 
+    // CRITICAL to parse not just use... or else brackets will fuck it all up
 	let packet1 = JSON.parse(pair[0]),
 		packet2 = JSON.parse(pair[1]),
 		maxlen = Math.max(packet1.length, packet2.length);
 
+    // increment the inOrder count accordingly 
 	if (maxlen < 1) inOrder += j + 1;
 
+    // obtain single values from each signal packet
 	for (let i = 0; i < maxlen; i++) {
 		let left = packet1[i],
 			right = packet2[i],
@@ -44,9 +56,6 @@ for (let j = 0; j < pairs.length; j++) {
 		if (i === maxlen - 1) inOrder += j + 1;
 	}
 }
-
-console.log(`Sum of indices of pairs in order: ${inOrder}`); // Part 1
-
 // Part 2
 const packets = pairs.flatMap((p) => p.split("\n")).concat(["[[2]]", "[[6]]"]);
 
